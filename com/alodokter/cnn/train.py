fix issue on RNN using manual matrix calculation

com/alodokter/cnn/train.py
@@ -23,6 +23,7 @@ tf.flags.DEFINE_string("filter_sizes", "3,4,5", "Comma-separated filter sizes (d
 tf.flags.DEFINE_integer("num_filters", 128, "Number of filters per filter size (default: 128)")
 tf.flags.DEFINE_float("dropout_keep_prob", 0.5, "Dropout keep probability (default: 0.5)")
 tf.flags.DEFINE_float("l2_reg_lambda", 0.5, "L2 regularization lambda (default: 0.0)")
+tf.flags.DEFINE_float("learning_rate", 0.01, "Learning Rate (default: 0.001)")
 
 # Training parameters
 tf.flags.DEFINE_integer("batch_size", 512, "Batch Size (default: 64)")
@@ -111,7 +112,10 @@ with tf.Graph().as_default():
 
         # Define Training procedure
         global_step = tf.Variable(0, name="global_step", trainable=False)
-        optimizer = tf.train.AdamOptimizer(1e-3)
+        # https://www.tensorflow.org/versions/r0.12/api_docs/python/train/decaying_the_learning_rate
+        starter_learning_rate = FLAGS.learning_rate
+        learning_rate = tf.train.exponential_decay(starter_learning_rate, global_step, 1000, 0.96, staircase=True)
+        optimizer = tf.train.AdamOptimizer(learning_rate)
         grads_and_vars = optimizer.compute_gradients(cnn.loss)
         train_op = optimizer.apply_gradients(grads_and_vars, global_step=global_step)
 

com/alodokter/rnn/text_classifier_rnn.py
@@ -16,7 +16,6 @@ class TextClassifierRNN:
         self.input_y = tf.placeholder(tf.float32, [None, num_classes], name="input_y")
         self.dropout_keep_prob = tf.placeholder(tf.float32, name="dropout_keep_prob")
         self.batch_size = tf.placeholder(tf.int32, [], name='batch_size')
-        self.seq_len = tf.placeholder(tf.int32, [None], name='seq_len')
 
         # Keeping track of l2 regularization loss (optional)
         l2_loss = tf.constant(0.0)
@@ -30,36 +29,56 @@ class TextClassifierRNN:
         cell = tf.contrib.rnn.GRUCell(num_units=embedding_size)
         cell = tf.contrib.rnn.DropoutWrapper(cell, output_keep_prob=self.dropout_keep_prob)
 
-        # initial_state = cell.zero_state(self.batch_size, tf.float32)
+        initial_state = cell.zero_state(self.batch_size, tf.float32)
         # Create an unrolled Recurrent Neural Networks to length of sequence_length
         # and passes inputs_series as inputs for each unit.
-        # _, encoding = tf.contrib.rnn.static_rnn(
-        #                         cell,
-        #                         inputs_series,
-        #                         sequence_length=self.length(inputs_series),
-        #                         dtype=tf.float32)
         _, encoding = tf.contrib.rnn.static_rnn(
-                            cell,
-                            inputs_series,
-                            dtype=tf.float32)
+                                cell,
+                                inputs_series,
+                                sequence_length=self.length(inputs_series),
+                                dtype=tf.float32)
 
+        # Final (unnormalized) scores and predictions
         with tf.name_scope("output"):
-            # self.scores = tf.layers.dense(encoding, num_classes, activation=None, name="scores")
-            regularizer = tf.contrib.layers.l2_regularizer(scale=l2_reg_lambda)
-            self.scores = tf.contrib.layers.fully_connected(
-                                    encoding,
-                                    num_classes,
-                                    weights_initializer = tf.contrib.layers.xavier_initializer(),
-                                    weights_regularizer = regularizer,
-                                    scope="scores")
+            W = tf.get_variable(
+                    "W",
+                    shape=[embedding_size, num_classes],
+                    initializer=tf.contrib.layers.xavier_initializer())
+            b = tf.get_variable(
+                    "b",
+                    initializer=tf.constant(0.1, shape=[num_classes]))
+            l2_loss += tf.nn.l2_loss(W)
+            l2_loss += tf.nn.l2_loss(b)
+            self.scores = tf.nn.xw_plus_b(encoding, W, b, name="scores")
             self.predictions = tf.argmax(self.scores, 1, name="predictions")
 
         # CalculateMean cross-entropy loss
         with tf.name_scope("loss"):
-            loss = tf.losses.softmax_cross_entropy(logits=self.scores, onehot_labels=self.input_y)
-            self.loss = loss + sum(tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES))
-            # losses = tf.nn.softmax_cross_entropy_with_logits(logits=self.scores, labels=self.input_y)
-            # self.loss = tf.reduce_mean(losses)
+            losses = tf.nn.softmax_cross_entropy_with_logits(logits=self.scores, labels=self.input_y)
+            self.loss = tf.reduce_mean(losses) + l2_reg_lambda * l2_loss
+
+        # _, encoding = tf.contrib.rnn.static_rnn(
+        #                     cell,
+        #                     inputs_series,
+        #                     dtype=tf.float32)
+
+        # with tf.name_scope("output"):
+        #     # self.scores = tf.layers.dense(encoding, num_classes, activation=None, name="scores")
+        #     regularizer = tf.contrib.layers.l2_regularizer(scale=l2_reg_lambda)
+        #     self.scores = tf.contrib.layers.fully_connected(
+        #                             encoding,
+        #                             num_classes,
+        #                             weights_initializer = tf.contrib.layers.xavier_initializer(),
+        #                             weights_regularizer = regularizer,
+        #                             scope="scores")
+        #     self.predictions = tf.argmax(self.scores, 1, name="predictions")
+
+        # # CalculateMean cross-entropy loss
+        # with tf.name_scope("loss"):
+        #     loss = tf.losses.softmax_cross_entropy(logits=self.scores, onehot_labels=self.input_y)
+        #     self.loss = loss + sum(tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES))
+        #     # losses = tf.nn.softmax_cross_entropy_with_logits(logits=self.scores, labels=self.input_y)
+        #     # self.loss = tf.reduce_mean(losses) + l2_reg_lambda * l2_loss
 
         # Accuracy
         with tf.name_scope("accuracy"):